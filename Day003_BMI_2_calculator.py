21ce43dc595bd1069b43a182168f8f1c920fb4f1
holgihe <holgihe@gmx.com>
2023-01-25 18:20:12 +0100

    Day003 added first part

---
+++ Day003_BMI_2_calculator.py
@@ -0,0 +1,52 @@
+#!/usr/bin/env python3
+####################################################################
+#           - Day003_BMI_2_calculator.py -
+#
+# Copyright (C) 2023 holgihe <holgihe@gmx.com>
+#
+####################################################################
+# Part of:  Udemy Course -100 Days of Code Python - Angela Yu
+#
+# Synopse:  Calculates Your body's BMI Body-Mass-Index
+#           ...
+# Input:    - Height (m)
+#           - Weight (kg)
+# Output:   - BMI with indication classification
+#           ..
+# Todo:     ..
+####################################################################
+# This program is free software; you can redistribute it and/or
+# modify it under the terms of the GNU General Public License
+# as published by the Free Software Foundation; either version 2
+# of the License, or (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this program.
+# If not, see <http://www.gnu.org/licenses/>.
+####################################################################
+
+# 🚨 Don't change the code below 👇
+height = float(input("enter your height in m: "))
+weight = float(input("enter your weight in kg: "))
+# 🚨 Don't change the code above 👆
+
+#Write your code below this line 👇
+
+bmi = weight / ((height ** 2))
+print (f"{weight} / ({height} x {height}) = {bmi}")
+
+if bmi <= 18.5:
+    print (f"Your BMI is {int(round(bmi,0))}, You are underweight.")
+elif bmi <= 25:
+    print (f"Your BMI is {int(round(bmi,0))}, You have normal weight.")
+elif bmi <=30:
+    print (f"Your BMI is {int(round(bmi,0))}, You are slightly overweight.")
+elif bmi <=35:
+    print (f"Your BMI is {int(round(bmi,0))}, You are obese.")
+else:
+    print (f"Your BMI is {int(round(bmi,0))}, You are clinically obese.")
---
+++ Day003_Odd_or_even.py
@@ -0,0 +1,41 @@
+#!/usr/bin/env python3
+####################################################################
+#           - Day003_Odd_or_even.py -
+#
+# Copyright (C) 2023 holgihe <holgihe@gmx.com>
+#
+####################################################################
+# Part of:  Udemy Course -100 Days of Code Python - Angela Yu
+#
+# Synopse:  Check if a given number is odd or even
+#           ...
+# Input:    - Number
+#           ..
+# Output:   - Answer (odd or even)
+# Todo:     ..
+####################################################################
+# This program is free software; you can redistribute it and/or
+# modify it under the terms of the GNU General Public License
+# as published by the Free Software Foundation; either version 2
+# of the License, or (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this program.
+# If not, see <http://www.gnu.org/licenses/>.
+####################################################################
+
+
+# 🚨 Don't change the code below 👇
+number = int(input("Which number do you want to check? "))
+# 🚨 Don't change the code above 👆
+
+#Write your code below this line 👇
+if number % 2 == 0 :
+    print ("This is an even number")
+else:
+    print ("This is an odd number")
